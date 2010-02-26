668ae1fa2af33376c0458492123eaa0810d8192f
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2010-02-26 16:07:42 +1300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import os
+
+setup(name='rfc3339',
+      version='0.1',
+      description='Python implementation of RFC 3339',
+      author='Tony Garnock-Jones',
+      author_email='tonyg@lshift.net',
+      url='http://www.lshift.net/',
+      py_modules=['rfc3339'],
+      )
