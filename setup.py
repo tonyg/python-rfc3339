c7861c2bba56dc8f1eb411c286c8def3398f6853
Chris Martin <ch.martin@gmail.com>
2015-12-04 16:29:30 -0800

    prepare setup.py for release named tonyg-rfc3339

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 import os
 
-setup(name='rfc3339',
+setup(name='tonyg-rfc3339',
       version='0.1',
       description='Python implementation of RFC 3339',
       author='Tony Garnock-Jones',
       author_email='tonyg@lshift.net',
-      url='http://www.lshift.net/',
+      url='https://github.com/cardforcoin/python-rfc3339',
       py_modules=['rfc3339'],
       )
