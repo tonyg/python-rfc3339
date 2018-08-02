e5c6e69ec848b734ab219759c4a39b0c9d9dbed3
Sergii Mikhtoniuk <smikhtoniuk@demonware.net>
2018-08-02 11:08:13 -0700

    Add parse_time and timetostr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import os
 
 setup(name='rfc3339',
-      version='0.1',
+      version='0.2',
       description='Python implementation of RFC 3339',
       author='Tony Garnock-Jones',
       author_email='tonyg@lshift.net',
