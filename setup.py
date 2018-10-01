9391e27e521cb1dd70f69d8f469a12e63fb7b116
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-10-01 18:18:42 +0100

    Adjust metadata to be consonant with the tonyg-rfc3339 package on pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 import os
 
-setup(name='rfc3339',
+setup(name='tonyg-rfc3339',
       version='0.2',
       description='Python implementation of RFC 3339',
       author='Tony Garnock-Jones',
-      author_email='tonyg@lshift.net',
-      url='http://www.lshift.net/',
+      author_email='tonygarnockjones@gmail.com',
+      url='https://github.com/tonyg/python-rfc3339',
       py_modules=['rfc3339'],
       classifiers=[
           'Programming Language :: Python :: 2',
